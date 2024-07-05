116016989e44fa6de037ce90498db5ff11de8854
ankitkumar0799 <ankitkuswaha07@gmail.com>
2024-07-05 14:54:59 +0530

    basic

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>hello ankit this side.</h1>
+     <h1>hllo ankit this side.</h1>
     </main>
   );
 }
